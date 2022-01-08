858189574e207e43a2404f818860cd2d395dd32f
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-01-08 23:14:10 +0900

    add

--- ABC/234/d.cpp
+++ ABC/234/d.cpp
@@ -1,3 +1,5 @@
+// tokenakatta
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,29 +9,19 @@ int main() {
   ll n, k;
   cin >> n >> k;
   ll p[500000];
-  for (ll i = 0; i < n; i++) {
-    cin >> p[i];
-  }
-  // i = k;
-  vector<ll> tmp;
-  for (ll i = 0; i < k; i++) {
-    tmp.push_back(p[i]);
-  }
-  sort(tmp.begin(), tmp.end(), std::greater<int>());
-  ll ret = tmp.at(k - 1);
-  std::cout << ret << std::endl;
+  for (ll i = 0; i < n; i++) cin >> p[i];
 
-  // k + 1 to n
+  priority_queue<ll, vector<ll>, greater<ll>> que;
+  for (size_t i = 0; i < k; i++) que.push(p[i]);
+
+  std::cout << que.top() << std::endl;
+
   for (ll i = k; i < n; i++) {
-    if (ret > p[i]) {
-      std::cout << ret << std::endl;
-      tmp.push_back(p[i]);
-    } else {
-      tmp.push_back(p[i]);
-      sort(tmp.begin(), tmp.end(), std::greater<int>());
-      ret = tmp.at(k - 1);
-      std::cout << ret << std::endl;
+    if (que.top() < p[i]) {
+      que.pop();
+      que.push(p[i]);
     }
+    std::cout << que.top() << std::endl;
   }
 
   return 0;
